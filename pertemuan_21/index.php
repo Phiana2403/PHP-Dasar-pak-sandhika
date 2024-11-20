c1de5ad7cb584a3e41a7bb14f86fa4548d207821
Phiana2403 <phiana1506@gmail.com>
2024-11-20 13:58:40 +0700

    php

--- pertemuan_21/index.php
+++ pertemuan_21/index.php
@@ -30,6 +30,12 @@ if( isset($_POST["cari"])){
             z-index: -1;
             display: none;
         }
+
+        @media print {
+            .logout, .tambah, .form-cari, .aksi{
+                display: none;
+            }
+        }
     </style>
 
     <script src="js/jquery-3.7.1.js"></script>
@@ -37,14 +43,14 @@ if( isset($_POST["cari"])){
 </head>
 <body>
 
-<a href="logout.php">Logout</a>
+<a href="logout.php" class="logout">Logout</a> | <a href="cetak.php" target="_blank">Cetak</a>
     
 <h1>Daftar Mahasiswa</h1>
 
-<a href="tambah.php">Tambah data mahasiswa</a>
+<a href="tambah.php" class="tambah">Tambah data mahasiswa</a>
 <br><br>
 
-<form action="" method="post">
+<form action="" method="post" class="form-cari">
 
     <input type="text" name="keyword" size="40" autofocus 
     placeholder="masukkan keyword pencarian.." autocomplete="off" id=
@@ -63,7 +69,7 @@ if( isset($_POST["cari"])){
         <!-- ctrl + d untuk mengubah td yang salah menjadi th
          tanpa harus manual satu-satu -->
         <th>No.</th>
-        <th>Aksi</th>
+        <th class="aksi">Aksi</th>
         <th>Gambar</th>
         <th>NRP</th>
         <th>Name</th>
@@ -75,7 +81,7 @@ if( isset($_POST["cari"])){
     <?php foreach( $mahasiswa as $row) : ?>
     <tr>
         <td><?= $i; ?></td>
-        <td>
+        <td class="aksi">
             <a href="ubah.php?id=<?= $row["id"]; ?>">Ubah</a>|
             <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm
             ('yakin?');">Hapus</a>
